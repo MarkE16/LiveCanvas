9e792c9c8b3c9d839074caa483862d4c566a3ea6
Mark Evola <markevola24@gmail.com>
2024-11-21 23:36:30 -0600

    fix locating tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		environment: "jsdom",
-		include: ["tests/frontend/**/*.test.tsx"],
+		include: ["tests/**/*.test.tsx"],
 		exclude: ["**/node_modules/**", "**/dist/**", "**/build/**"],
 		globals: true,
 	},
