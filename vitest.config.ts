cbc38febcf4a6dd1c6b98f18ebf6013a4f12d065
Mark Evola <markevola24@gmail.com>
2025-05-09 23:24:28 -0500

    update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		environment: "jsdom",
-		include: ["tests/**/*.test.ts", "tests/**/*.test.tsx"],
+		include: ["src/tests/**/*.test.ts", "src/tests/**/*.test.tsx"],
 		exclude: [
 			"**/node_modules/**",
 			"**/dist/**",
