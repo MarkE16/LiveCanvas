5c4a29fd8399dfda4ca883e6d2c64995b6b0b296
Mark Evola <markevola24@gmail.com>
2025-10-08 13:36:38 -0500

    fix(previews): fix layer previews

--- src/state/slices/canvasSlice.ts
+++ src/state/slices/canvasSlice.ts
@@ -490,6 +490,8 @@ export const createCanvasSlice: StateCreator<
 			return aPosition - bPosition;
 		});
 
+		ctx.clearRect(0, 0, baseCanvas.width, baseCanvas.height);
+
 		if (!options?.preview) {
 			const rect = DOMCanvas.getBoundingClientRect();
 
